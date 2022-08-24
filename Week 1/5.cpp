22d41a811bed19b60ef540bd408f5780f1fad2f2
Hrithik2212 <hr6466@srmist.edu.in>
2022-08-24 20:55:40 +0530

    Week 1

--- Week 1/5.cpp
+++ Week 1/5.cpp
@@ -1,9 +1,10 @@
 #include <iostream> 
 using namespace ::std ;
-/* WAP to calculate total number of notes*/
+/* WAP to print from 1-10 without 6 by using one for loop */
 int main(){
-    int n,m,l;
-    cout << "The Amount : ";
-    cin >> n;
-    m = 
+    for (int i=1; i<=10; i++){
+        if (i!=6){
+            cout << i << endl ; 
+        }
+    }
 }
